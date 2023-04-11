83f494051614c058992a6b992eefdb9b05164329
tropicdev <jordan.bmp.mckoy@gmail.com>
2023-04-11 03:12:12 +0000

    Changed Wording

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -387,7 +387,7 @@ function ActionButton(row: Row) {
                                                             }}
                                                             className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
                                                         >
-                                                            Add to bag
+                                                            Add to List
                                                         </button>
                                                     </div>
                                                     <div className="mt-6 text-center">
