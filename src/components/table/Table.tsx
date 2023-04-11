54e5f723593f179296f753b35b2f8e20fb8d17e4
tropicdev <jordan.bmp.mckoy@gmail.com>
2023-04-11 03:10:47 +0000

    Added x Button

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -288,7 +288,10 @@ function ActionButton(row: Row) {
                                         <button
                                             type="button"
                                             className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
-                                            onClick={() => setOpen(false)}
+                                            onClick={() => {
+                                                setOpen(false)
+                                                row.set?.(null)
+                                            }}
                                         >
                                             <span className="sr-only">Close</span>
                                             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
