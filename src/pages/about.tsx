f88a5197a16ce314fcbf16dca707ba9dd022f570
jordan.bmp.mckoy <jordan.bmp.mckoy@gmail.com>
2023-04-17 19:36:33 -0500

    Finished

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -90,7 +90,7 @@ export default function About() {
                 {/* Hero section */}
                 <div className="relative isolate -z-10">
                     <svg
-                        className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+                        className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-600 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
                         aria-hidden="true"
                     >
                         <defs>
